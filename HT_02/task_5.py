a5350f5ad8d91a7e1d78c4a42a6f0098fd4c73ea
Stanislav <nstanislass@gmail.com>
2023-10-18 00:20:47 +0300

    line 4: add text

--- HT_02/task_5.py
+++ HT_02/task_5.py
@@ -1,5 +1,5 @@
 # Write a script which accepts decimal number from user and converts it to hexadecimal.
 
 
-user_input = int(input())
+user_input = int(input('Enter decimal number: '))
 print(hex(user_input))
