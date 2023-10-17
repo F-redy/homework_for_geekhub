ded0f179dbb6b93d1621df4d15e5c191316ae3e5
Stanislav <nstanislass@gmail.com>
2023-10-18 00:11:37 +0300

    line 8: add text

--- HT_02/task_3.py
+++ HT_02/task_3.py
@@ -5,7 +5,7 @@ def get_sum_numbers(list_numbers: list[int], user_number: int) -> int:
     return sum(list_numbers[:user_number])
 
 
-n = int(input())
+n = int(input('Enter number: '))
 numbers = [i for i in range(n)]
 
 sum_numbers = get_sum_numbers(list_numbers=numbers, user_number=n)
