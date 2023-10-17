0d11df520bc61d2a66eed0a5a26a19ed15d5be19
Stanislav <nstanislass@gmail.com>
2023-10-17 22:53:50 +0300

    add task 6

---
+++ HT_02/task_6.py
@@ -0,0 +1,15 @@
+"""
+    Write a script to check whether a value from user input is contained in a group of values.
+        e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
+             [1, 2, 'u', 'a', 4, True] --> 5 --> False
+"""
+
+
+def check_value_in_group(value: str, group: list):
+    return value in group
+
+
+user_value = input()
+group_values = [1, 2, 'u', 'a', 4, True]
+
+print(check_value_in_group(user_value, group_values))
