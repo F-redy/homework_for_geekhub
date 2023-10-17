920d53c37cfe1e1e9fa75b5d0a8bae8f8196584e
Stanislav <nstanislass@gmail.com>
2023-10-17 22:38:22 +0300

    add task 4

---
+++ HT_02/task_4.py
@@ -0,0 +1,14 @@
+"""
+    Write a script which accepts a <number> from user and then <number> times asks user for string input.
+    At the end script must print out result of concatenating all <number> strings.
+"""
+
+
+def concatenate_strings(user_data: list[str]) -> str:
+    return ''.join(user_data)
+
+
+user_input = int(input())
+data = [input() for _ in range(user_input)]
+
+print(concatenate_strings(data))
