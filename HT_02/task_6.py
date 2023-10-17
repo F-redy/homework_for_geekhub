069ee989a8cc6f90bb786edf67f3236aa32ccabd
Stanislav <nstanislass@gmail.com>
2023-10-18 00:28:19 +0300

    fix check_value_in_group; line 10: add text

--- HT_02/task_6.py
+++ HT_02/task_6.py
@@ -3,11 +3,11 @@
 #      [1, 2, 'u', 'a', 4, True] --> 5 --> False
 
 
-def check_value_in_group(value: str, group: list):
-    return value in group
+def check_value_in_group(value: str, group: list) -> bool:
+    return value in map(str, group)
 
 
-user_value = input()
+user_value = input('Enter the search value: ')
 group_values = [1, 2, 'u', 'a', 4, True]
 
 print(check_value_in_group(user_value, group_values))
