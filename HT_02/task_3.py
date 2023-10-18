dea2d2041b42869a5e729f4a3f460841d8b0a274
Stanislav <nstanislass@gmail.com>
2023-10-18 15:29:43 +0300

    make easy

--- HT_02/task_3.py
+++ HT_02/task_3.py
@@ -1,13 +1,6 @@
 # Write a script which accepts a <number> from user and print out a sum of the first <number> positive integers.
 
 
-def get_sum_numbers(list_numbers: list[int], user_number: int) -> int:
-    return sum(list_numbers[:user_number])
-
-
 n = int(input('Enter number: '))
-numbers = list(range(1, n + 1))
 
-sum_numbers = get_sum_numbers(list_numbers=numbers, user_number=n)
-
-print(sum_numbers)
+print(sum(range(n + 1)))
