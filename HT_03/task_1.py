8227f4cfa7e5b4c64b30c3246cbba839d9fa6412
Stanislav <nstanislass@gmail.com>
2023-10-19 23:04:29 +0300

    add task 1

---
+++ HT_03/task_1.py
@@ -0,0 +1,13 @@
+# 1. Write a script that will run through a list of tuples and replace the last value for each tuple.
+# The list of tuples can be hardcoded. The "replacement" value is entered by user.
+# The number of elements in the tuples must be different.
+
+
+from random import randint
+
+random_number_tuples = [tuple(randint(0, 100) for _ in range(randint(3, 7))) for _ in range(randint(5, 10))]
+
+user_value = int(input('Enter the number to replace: '))
+random_number_tuples = [tuple(t[:-1] + (user_value,)) for t in random_number_tuples]
+
+print(random_number_tuples)
