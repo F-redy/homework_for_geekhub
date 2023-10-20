2c2a563ab8fdca0d3332b2aa4897b5c3f0019856
Stanislav <nstanislass@gmail.com>
2023-10-20 11:18:08 +0300

    fix line 13: add if

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -6,8 +6,10 @@
 from random import randint
 
 random_number_tuples = [tuple(randint(0, 100) for _ in range(randint(3, 7))) for _ in range(randint(5, 10))]
+print(f'Started list:\n{random_number_tuples}\n')
 
 user_value = int(input('Enter the number to replace: '))
-random_number_tuples = [tuple(t[:-1] + (user_value,)) for t in random_number_tuples]
 
-print(random_number_tuples)
+random_number_tuples = [tuple(t[:-1] + (user_value,)) for t in random_number_tuples if t]
+
+print(f'\nChanged list:\n{random_number_tuples}')
