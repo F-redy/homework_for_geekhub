b009ca7b81c4a778fb3e42480bca20f3ea3a5cfa
Stanislav <nstanislass@gmail.com>
2023-10-22 19:02:12 +0300

    delete function concatenate_strings

--- HT_02/task_4.py
+++ HT_02/task_4.py
@@ -2,11 +2,7 @@
 # At the end script must print out result of concatenating all <number> strings.
 
 
-def concatenate_strings(user_data: list[str]) -> str:
-    return ''.join(user_data)
-
-
 user_input = int(input('Enter number: '))
 data = [input(f'Enter string №{i + 1}: ') for i in range(user_input)]
 
-print(concatenate_strings(data))
+print(''.join(data))
