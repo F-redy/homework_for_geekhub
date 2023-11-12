4116e42cf0633b887b7d78b21971e3fe93fcafa1
Stanislav <nstanislass@gmail.com>
2023-11-12 20:09:06 +0200

    add task 1

---
+++ HT_09/task_1.py
@@ -0,0 +1,96 @@
+# task 1.
+# Програма-світлофор.
+#    Створити програму-емулятор світлофора для авто і пішоходів.
+#    Після запуска програми на екран виводиться в лівій половині - колір автомобільного,
+#    а в правій - пішохідного світлофора. Кожну 1 секунду виводиться поточні кольори.
+#    Через декілька ітерацій - відбувається зміна кольорів - логіка така сама як і в звичайних світлофорах
+#    (пішоходам зелений тільки коли автомобілям червоний).
+#    Приблизний результат роботи наступний:
+#       Red        Green
+#       Red        Green
+#       Red        Green
+#       Red        Green
+#       Yellow     Red
+#       Yellow     Red
+#       Green      Red
+#       Green      Red
+#       Green      Red
+#       Green      Red
+#       Yellow     Red
+#       Yellow     Red
+#       Red        Green
+
+
+from time import sleep
+
+from servises.text_colorize import colorize_text
+
+SETTINGS = {
+    'padding between text': 15,
+    'headers text': f'CAR {"":^16}PEOPLE'
+}
+
+
+def get_colors():
+    return [colorize_text(color.capitalize(), color) for color in ['red', 'green', 'yellow']]
+
+
+def print_colors(car_color: str, people_color: str, repeat: int = 4, timer_sleep: int = 1) -> None:
+    """
+    :param car_color: traffic light color for cars
+    :param people_color: traffic light color for people
+    :param repeat: Number of times to repeat the print.
+    :param timer_sleep: Sleep duration between prints.
+
+    """
+    __padding = SETTINGS['padding between text']
+
+    for _ in range(repeat):
+        print(f'{car_color:<{__padding}}{"":>{__padding}}'
+              f'{people_color}')
+        sleep(timer_sleep)
+
+
+def traffic_light():
+    """
+    Simulates a traffic light sequence by printing colored lines representing the traffic light state.
+
+    The sequence consists of the following steps:
+    1. Cars: Red -> People: Green; default (repeated four times)
+    2. Cars: Yellow -> People: Red (repeated twice)
+    3. Cars: Green -> People: Red; default (repeated four times)
+
+    The function uses the colors defined in the `get_colors` function and sets the padding between text based on
+    the value stored in SETTINGS['padding between text'].
+
+    This function provides a visual representation of a traffic light sequence with specified sleep durations between
+    each step.
+
+    Example:
+    traffic_light()
+    """
+    red, green, yellow = get_colors()
+
+    print_colors(red, green)
+    print_colors(yellow, red, repeat=2)
+    print_colors(green, red)
+
+
+def crossroads(repeat: int = 2):
+    """
+    Simulates a traffic light sequence at a crossroads with a specified number of repetitions.
+
+    This func prints a header text followed by a series of traffic light sequences, simulating a crossroads scenario.
+    The traffic light sequence is defined by the 'traffic_light' function.
+
+    :param repeat: Number of times to repeat the traffic light sequence at the crossroads.
+    """
+    print(f'{SETTINGS["headers text"]}'
+          f'\n{"-" * len(SETTINGS["headers text"])}')
+    while repeat:
+        traffic_light()
+        repeat -= 1
+
+
+if __name__ == '__main__':
+    crossroads()
