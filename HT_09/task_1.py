761817abc7734e7734125668615e9d7ac6b48014
Stanislav <nstanislass@gmail.com>
2023-11-13 13:19:23 +0200

    add colorize_text

--- HT_09/task_1.py
+++ HT_09/task_1.py
@@ -23,12 +23,43 @@
 
 from time import sleep
 
-from servises.text_colorize import colorize_text
-
 SETTINGS = {
     'padding between text': 15,
     'headers text': f'CAR {"":^16}PEOPLE'
 }
+
+COLORS = {
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'orange': '\033[31;2m',
+    'blue': '\033[34m',
+    'violet': '\033[35m',
+    'light_blue': '\033[36m',
+    'drop_color': '\033[0m'
+}
+
+
+class UnsupportedColorError(Exception):
+    pass
+
+
+def colorize_text(text: str, color: str) -> str:
+    """
+    The function paints the input text in the specified color.
+
+    Available colors:
+    - 'green'     : Green color
+    - 'red'       : Red color
+    - 'yellow'    : Yellow color
+    - 'blue'      : Blue color
+    - 'violet'    : Violet color
+    - 'light_blue': Light blue color
+    """
+    if color in COLORS:
+        return f"{COLORS[color]}{text}{COLORS['drop_color']}"
+    else:
+        raise UnsupportedColorError(f'Don\'t have color: {color}')
 
 
 def get_colors():
