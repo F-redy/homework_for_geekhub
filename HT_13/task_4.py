ef4b64fc79d737c4374fba09f737cbcf535b8c46
Stanislav <nstanislass@gmail.com>
2023-11-30 13:54:49 +0200

    fix negative index add setitem and delitem

--- HT_13/task_4.py
+++ HT_13/task_4.py
@@ -4,23 +4,44 @@
 # але індексація повинна починатись із 1
 
 class CustomList(list):
-    def __getitem__(self, item):
-        if not item:
-            raise IndexError('Бо не можна!')
-        return super().__getitem__(item - 1)
+    def __getitem__(self, index):
+        if not index:
+            raise IndexError('Call __getitem__. Бо не можна!')
+        return super().__getitem__((index, index - 1)[index > 0])
+
+    def __setitem__(self, index, value):
+        if not index:
+            raise IndexError('Call __setitem__. Бо не можна!')
+        return super().__setitem__((index, index - 1)[index > 0], value)
+
+    def __delitem__(self, index):
+        if not index:
+            raise IndexError('Call __delitem__. Бо не можна!')
+        return super().__delitem__((index, index - 1)[index > 0])
 
 
 if __name__ == '__main__':
     lst = CustomList(range(20))
     print(lst)
+    print(lst[-1])
     tmp = lst.pop()
     lst[5] = 'халабуда'
     print(lst)
-    lst[0] = tmp
-    print(lst)
     lst[5] = lst.pop(7)
+    print(lst)
+    lst.insert(-1, 20)
     print(lst)
     try:
         print(lst[0])
     except IndexError as e:
         print(e)
+    try:
+        lst[0] = tmp
+    except IndexError as e:
+        print(e)
+    try:
+        del lst[0]
+    except IndexError as e:
+        print(e)
+    print(lst)
+    print(len(lst))
