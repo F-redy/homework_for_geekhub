a68d35f501665a00ffd3290e672f2137c69ebceb
Stanislav <nstanislass@gmail.com>
2024-01-01 13:43:03 +0200

    fix line 5

--- HT_12/atm_3_0/settings.py
+++ HT_12/atm_3_0/settings.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.path.dirname(__file__)
 
-DB_PATH = DATA_USERS = os.path.join(ROOT_DIR, 'atm.db')
+DB_PATH = os.path.join(ROOT_DIR, 'atm.db')
 SCHEMA_FILE_PATH = os.path.join(ROOT_DIR, 'db_scripts.sql')
 
 ALLOWED_CURRENCY = (10, 20, 50, 100, 200, 500, 1000)
