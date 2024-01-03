0ad3612feaf5aff4aca82d48ccf3f9b87505d0a0
Stanislav <nstanislass@gmail.com>
2024-01-03 20:03:21 +0200

    change line 12: add AddProductsFormView delete add_products

--- HT_18/sears/apps/products/urls.py
+++ HT_18/sears/apps/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.products.views import add_products
+from apps.products.views import AddProductsFormView
 from apps.products.views import ProductDetailView
 from apps.products.views import ProductListView
 from apps.products.views import task
@@ -9,7 +9,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='my_products'),
-    path('products/add-products/', add_products, name='add_products'),
+    path('products/add-products/', AddProductsFormView.as_view(), name='add_products'),
     path('products/detail-product/<str:product_id>', ProductDetailView.as_view(), name='detail_product'),
     path('task/', task, name='task'),
 ]
